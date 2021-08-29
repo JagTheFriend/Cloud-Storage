920775d1b491ac6820ed69241b4d1826fa6b0f53
JagTheFriend <jagthefriend12@gmail.com>
2021-08-29 17:45:50 +0200

    Updated model.py

--- website/models.py
+++ website/models.py
@@ -5,5 +5,6 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     # max length of 150 characters, and it must be unique
+    email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)  # username
     password = db.Column(db.String(150))  # doesn't need to be unique

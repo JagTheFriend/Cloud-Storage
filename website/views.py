b506cc63f090ed9299166e7abab0751b334f83ee
JagTheFriend <jagthefriend12@gmail.com>
2021-08-31 23:01:44 +0200

    Working on the home page

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from . import log
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
@@ -10,4 +10,4 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     log.debug("Received a GET request at `/home`")
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)

0ed0254e74279b4c03815f1cc91f007af335b7ac
JagTheFriend <jagthefriend12@gmail.com>
2021-09-01 10:08:14 +0200

    Working on a way to get files uploaded by the user

--- website/views.py
+++ website/views.py
@@ -14,11 +14,15 @@ def home():
     return render_template("home.html", user=current_user)
 
 
-@views.route("/files?username=<string:username>")
+@views.route("/files/username=<username>")
+# @login_required
 def get_files(username: str):
     # check whether the folder already exists
+    files = {}
     if os.path.isdir(f"{DATABASE}/{username}"):
-        return "<h1> yes </h1>"
+        os.chdir(f"{DATABASE}/{username}")
+        files = {i: f"{DATABASE}/{username}/{i}" for i in os.listdir()}
     else:
         # creating the folder
         os.mkdir(f"{DATABASE}/{username}")
+    return files

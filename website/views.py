60dfccd836752d8a4a94ffc95ba1e27077b1bcf1
JagTheFriend <jagthefriend12@gmail.com>
2021-09-01 21:55:39 +0200

    Added a way to show the (uploaded) files

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from . import DATABASE, log
 import os
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
@@ -19,10 +19,22 @@ def home():
 def get_files(username: str):
     # check whether the folder already exists
     files = {}
-    if os.path.isdir(f"{DATABASE}/{username}"):
-        os.chdir(f"{DATABASE}/{username}")
-        files = {i: f"{DATABASE}/{username}/{i}" for i in os.listdir()}
+    if os.path.isdir(f"{DATABASE}\\{username}"):
+        os.chdir(f"{DATABASE}\\{username}")
+        files = {i: f"{DATABASE}\\{username}\\{i}" for i in os.listdir()}
     else:
         # creating the folder
         os.mkdir(f"{DATABASE}/{username}")
     return files
+
+
+@views.route("/get_file/filename=<filename>&username=<username>")
+@login_required
+def get_specific_file(filename: str, username: str):
+    if os.path.isdir(f"{DATABASE}\\{username}"):
+        os.chdir(f"{DATABASE}\\{username}")
+        if os.path.isfile(filename):
+            with open(filename, "r") as file:
+                return file.read()
+    else:
+        flash("File not found!", category="error")
